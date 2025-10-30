2ddd220762cafd83a0fb10bf78ef897b8111f30d
GuillaumedeChilly <guillaumedechilly@gmail.com>
2025-10-30 16:19:23 +0000

    fix(atoi): pointer-only version, no array syntax, Betty clean

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,55 +2,48 @@
 #include <limits.h>
 
 /**
- * _atoi - converts a string to an integer.
- * @s: string to convert.
+ * _atoi - converts a string to an integer
+ * @s: string to convert
  *
- * Return: converted number, clamped to INT_MAX/INT_MIN on overflow,
- *         or 0 if no digits are found.
+ * Return: integer value (0 if no digits found)
  */
 int _atoi(char *s)
 {
-	int i = 0;
 	int sign = 1;
 	int res = 0;
 	int started = 0;
 
-	/* Walk prefix: flip sign on '-', start when we hit a digit */
-	while (s[i] != '\0' && !started)
+	/* Skip non-digit chars, track sign */
+	while (*s && !started)
 	{
-		if (s[i] == '-')
+		if (*s == '-')
 			sign = -sign;
-		else if (s[i] >= '0' && s[i] <= '9')
+		else if (*s >= '0' && *s <= '9')
 			started = 1;
-		i++;
+		s++;
 	}
 
 	if (!started)
 		return (0);
+	s--; /* step back to first digit */
 
-	i--; /* step back to first digit we saw */
-
-	/* Convert digits with overflow protection */
-	while (s[i] >= '0' && s[i] <= '9')
+	while (*s >= '0' && *s <= '9')
 	{
-		int d = s[i] - '0';
+		int d = *s - '0';
 
 		if (sign == 1)
 		{
-			/* res * 10 + d > INT_MAX ? */
 			if (res > (INT_MAX - d) / 10)
 				return (INT_MAX);
 			res = res * 10 + d;
 		}
 		else
 		{
-			/* res * 10 - d < INT_MIN ?  (res is kept negative here) */
 			if (res < (INT_MIN + d) / 10)
 				return (INT_MIN);
 			res = res * 10 - d;
 		}
-		i++;
+		s++;
 	}
-
 	return (res);
 }
