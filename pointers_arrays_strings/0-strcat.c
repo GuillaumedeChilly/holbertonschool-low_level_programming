4f26b8ac574c86cc47473759a30bb7243d60a4ad
GuillaumedeChilly <guillaumedechilly@gmail.com>
2025-10-30 15:18:33 +0000

    pointers_arrays_strings: fix Betty style (tabs, trailing spaces), add README, place files correctly

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,43 @@
 #include "main.h"
+
 /**
  * _strcat - concatenates two strings
  * @dest: destination buffer
  * @src: source string
- * Return: dest
+ *
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-        i++;
-    while (src[j] != '\0')
-    {
-        dest[i + j] = src[j];
-        j++;
-    }
-    dest[i + j] = '\0';
-    return (dest);
+	while (dest[i] !
+
+cat > 0-strcat.c << 'EOF'
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+
+	dest[i + j] = '\0';
+	return (dest);
 }
