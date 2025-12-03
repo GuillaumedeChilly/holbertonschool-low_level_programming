870ab451e5153474d5d9ae5b38c62b537d279a50
guillaumedechilly <guillaumedechilly@gmail.com>
2025-12-03 15:56:53 +0000

    Add hash tables project tasks 0-6

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,60 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds or updates an element in a hash table
+ * @ht: hash table
+ * @key: key (cannot be empty)
+ * @value: value associated with the key (will be duplicated)
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *tmp;
+	char *value_copy;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			value_copy = strdup(value);
+			if (value_copy == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = value_copy;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,6 @@
+# Hash tables
+
+Project implementing hash tables in C:
+- djb2 hash function
+- key to index mapping
+- setting, getting, printing and deleting entries
